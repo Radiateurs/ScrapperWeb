8b868f7068ce8c2241619c3b319aafa34fcde3d8
Pierre-Marie (Radiateurs) <pierre-marie.danieau@epitech.eu>
2017-08-18 22:59:26 +0200

    1st version. Modify data in main to get what you want

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Scrapper scrapper = new Scrapper("https://9gag.com/");
+
+            scrapper.Scrap(".jpg");
+            scrapper.ExportDatas("D:\\pictures\\test9gag");
+        }
+    }
+}
---
+++ WindowsFormsApp1/Scrapper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Scrapper
+    {
+        private Uri addr;
+        private HttpWebRequest request;
+        private HttpWebResponse response;
+        public List<string> datas;
+
+        public Scrapper()
+        {
+        }
+
+        public Scrapper(string URL)
+        {
+            addr = new Uri(URL);
+            datas = new List<string>();
+        }
+
+        public void Scrap(string wish)
+        {
+            string RowData;
+            string tmp;
+            int start, end, lastPos;
+            CookieContainer cookie = new CookieContainer();
+
+            request = (HttpWebRequest)WebRequest.Create(addr);
+            request.Method = "GET";
+            request.CookieContainer = cookie;
+            request.Credentials = CredentialCache.DefaultCredentials;
+            response = (HttpWebResponse)request.GetResponse();
+            Stream received = response.GetResponseStream();
+            StreamReader reader = new StreamReader(received, Encoding.UTF8);
+            RowData = reader.ReadToEnd();
+            Console.WriteLine(RowData);
+            lastPos = 0;
+            while (RowData.Contains(wish))
+            {
+                start = RowData.IndexOf(wish, lastPos);
+                end = start + wish.Length - 1;
+                while (RowData[start] != '\"' && start > 0)
+                    start--;
+                if (start == 0)
+                    break;
+                tmp = RowData.Substring(start + 1, end - start);
+                Console.WriteLine(tmp);
+                if (tmp.Substring(0, 4).Contains("http") || tmp.Substring(0, 5).Contains("https"))
+                    datas.Add(tmp);
+                RowData = RowData.Substring(end, RowData.Length - end);
+            }
+            Console.WriteLine("Founded {0} matches", datas.Count);
+            response.Close();
+        }
+
+        public void ExportDatas(string folderPath)
+        {
+            string completedPath;
+            WebClient wc = new WebClient();
+
+            foreach (string row in datas)
+            {
+                Console.WriteLine(row);
+                completedPath = folderPath + "\\" + row.Substring(row.LastIndexOf('/') + 1);
+                wc.DownloadFile(row, completedPath);
+            }
+        }
+    }
+}
